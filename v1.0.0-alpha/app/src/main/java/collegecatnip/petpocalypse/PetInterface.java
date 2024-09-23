34765bde185312e18c19e671fef6d2f33b1c7172
JacktheDog617 <jackandjaime10@gmail.com>
2024-09-23 18:41:48 -0400

    Add files via upload

---
+++ v1.0.0-alpha/app/src/main/java/collegecatnip/petpocalypse/PetInterface.java
@@ -0,0 +1,31 @@
+package collegecatnip.petpocalypse;
+/**
+ * Pet interface to sync cat and dog classes
+ *
+ * @author Kendall Devich
+ * Date Created: 9/23/2024
+ *
+ * @author Kendall Devich
+ * Last Modified: 9/23/2024
+ * Patch Notes:
+ *		The base of the pet class.
+ */
+
+public interface PetInterface
+{
+    // Getters and Setters
+    public String getNickName();
+
+    public Void setNickName(String name);
+
+    public String getBreed();
+
+    public String getFlavorText();
+
+    public int getAction();
+
+    public int getRarity();
+
+    public int getPettionaryID();
+
+}

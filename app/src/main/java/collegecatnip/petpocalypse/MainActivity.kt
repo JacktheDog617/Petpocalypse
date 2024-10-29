4204de423e28bc922e40e31f264ad301f1d7bc43
Jaime Lee <jackandjaime10@gmail.com>
2024-10-28 23:18:58 -0400

    Successfully fixed buttons

--- app/src/main/java/collegecatnip/petpocalypse/MainActivity.kt
+++ app/src/main/java/collegecatnip/petpocalypse/MainActivity.kt
@@ -1,16 +1,14 @@
 package collegecatnip.petpocalypse
 
-import android.R.attr.onClick
-import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,12 +18,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-    }
 
-    private fun configureShopButton(){
-        val shopButton = findViewById<ImageButton>(R.id.shop_button);
+        val shopButton: ImageButton = findViewById(R.id.shop_button)
         shopButton.setOnClickListener {
-            startActivity(Intent(this, ShopActivity::class.java).also{startActivity(it)})
             val intent = Intent(this, ShopActivity::class.java)
             startActivity(intent)
         }
--- app/src/main/java/collegecatnip/petpocalypse/ShopActivity.kt
+++ app/src/main/java/collegecatnip/petpocalypse/ShopActivity.kt
@@ -2,31 +2,19 @@ package collegecatnip.petpocalypse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageButton
-import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
 import collegecatnip.petpocalypse.ui.petbox.PetBoxFragment
 import android.content.Intent
 import android.widget.Button
 
-class ShopActivity : ComponentActivity() {
+class ShopActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_shop)
-//        if (savedInstanceState == null) {
-//            supportFragmentManager.beginTransaction()
-//                .replace(R.id.container, PetBoxFragment.newInstance())
-//                .commitNow()
-//        }
-    }
-
-    private fun configureBackButton(){
-        val backButton = findViewById<Button>(R.id.back_button);
-        backButton.setOnClickListener {
-            finish()
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, PetBoxFragment.newInstance())
+                .commitNow()
         }
 
         val backButton: Button = findViewById(R.id.backButton)
